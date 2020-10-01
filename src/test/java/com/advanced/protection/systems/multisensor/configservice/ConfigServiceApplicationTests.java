19ae7370afc6af169b55fe14e5dc905ee288ebe8
lukaszp <L.Ptaszek@tt.com.pl>
2020-10-01 06:45:03 +0200

    basic config for gateway

---
+++ src/test/java/com/advanced/protection/systems/multisensor/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advanced.protection.systems.multisensor.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
